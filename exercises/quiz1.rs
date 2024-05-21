841b7a8ce26cafc516a1de91aa8071c8491e2a34
brian <lbc.brian.chilin@gmail.com>
2024-05-21 12:06:14 -0700

    through quiz 1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
 // fn calculate_price_of_apples {
+
+fn calculate_price_of_apples(raw: u32) -> u32 {
+    if raw < 41 {
+        return raw*2
+    }
+    raw
+}
 
 // Don't modify this function!
 #[test]
