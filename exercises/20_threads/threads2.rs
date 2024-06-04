bc792d6c07ced369de14c07ae9ebb1f67914b734
brian <lbc.brian.chilin@gmail.com>
2024-06-04 02:18:17 -0700

    threads

--- exercises/20_threads/threads2.rs
+++ exercises/20_threads/threads2.rs
@@ -7,11 +7,10 @@
 // Execute `rustlings hint threads2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::sync::Arc;
 use std::thread;
 use std::time::Duration;
+use std::sync::Mutex;
 
 struct JobStatus {
     jobs_completed: u32,
@@ -19,7 +18,7 @@ struct JobStatus {
 
 fn main() {
     // TODO: `Arc` isn't enough if you want a **mutable** shared state
-    let status = Arc::new(JobStatus { jobs_completed: 0 });
+    let status = Arc::new(Mutex::new(JobStatus { jobs_completed: 0 }));
 
     let mut handles = vec![];
     for _ in 0..10 {
@@ -27,7 +26,9 @@ fn main() {
         let handle = thread::spawn(move || {
             thread::sleep(Duration::from_millis(250));
             // TODO: You must take an action before you update a shared value
-            status_shared.jobs_completed += 1;
+            let mut mut_shared = status_shared.lock().unwrap();
+            //status_shared.jobs_completed += 1;
+            mut_shared.jobs_completed += 1;
         });
         handles.push(handle);
     }
@@ -38,5 +39,5 @@ fn main() {
     }
 
     // TODO: Print the value of `JobStatus.jobs_completed`
-    println!("Jobs completed: {}", ???);
+    println!("Jobs completed: {}", Arc::clone(&status).lock().unwrap().jobs_completed);
 }
