f4a3f66abd998bf393eaa02a084cc9a10fde3549
brian <lbc.brian.chilin@gmail.com>
2024-06-06 19:08:52 -0700

    finish with ch23

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -41,10 +41,29 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
-    fn from(s: &str) -> Person {}
+    fn from(s: &str) -> Person {
+        println!("{}", s);
+
+        if s.len() == 0 {
+            return Person::default()
+        }
+        
+        let attributes: Vec<&str> = s.split(',').collect();
+        if attributes.len() != 2 {
+            return Person::default()
+        }
+
+        if attributes[0].len() == 0 {
+            return Person::default()
+        }
+        match attributes[1].parse::<usize>() {
+            Ok(n) => {return Person{age: n, name: attributes[0].to_string()}},
+            Err(_) => {return Person::default()}
+        }
+        
+
+    }
 }
 
 fn main() {
--- exercises/23_conversions/from_str.rs
+++ exercises/23_conversions/from_str.rs
@@ -31,8 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -48,6 +46,22 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty)
+        }
+
+        let attributes: Vec<&str> = s.split(',').collect();
+        if attributes.len() != 2 {
+            return Err(ParsePersonError::BadLen)
+        }
+        if attributes[0].len() == 0 {
+            return Err(ParsePersonError::NoName)
+        }
+        match attributes[1].parse::<usize>() {
+            Ok(n) => {return Ok(Person{age: n, name: attributes[0].to_string()})},
+            Err(e) => {return Err(ParsePersonError::ParseInt(e))}
+        }
+
     }
 }
 
